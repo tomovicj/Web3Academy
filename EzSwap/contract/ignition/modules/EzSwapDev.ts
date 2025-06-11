899d8a30517ef03c31c6b768ac9f3a865b306c8b
Tomovic <jovantomovic5@gmail.com>
2025-06-12 01:47:58 +0200

    EzSwap: updated contracts deployment script for dev env

--- EzSwap/contract/ignition/modules/EzSwapDev.ts
+++ EzSwap/contract/ignition/modules/EzSwapDev.ts
@@ -2,7 +2,12 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("EzSwapDev", (m) => {
   const factoryContract = m.contract("EzSwapFactory", []);
-  const token0Contract = m.contract("MockERC20", ["Token1", "T1", 18], {id: "MockToken1"});
-  const token1Contract = m.contract("MockERC20", ["Token2", "T2", 18], {id: "MockToken2"});
-  return { factoryContract, token0Contract, token1Contract };
+
+  const token0Contract = m.contract("MockERC20", ["Token1", "T1", 18], { id: "MockToken1" });
+
+  const token1Contract = m.contract("MockERC20", ["Token2", "T2", 18], { id: "MockToken2" });
+
+  const token2Contract = m.contract("MockERC20", ["Token3", "T3", 18], { id: "MockToken3" });
+
+  return { factoryContract, token0Contract, token1Contract, token2Contract };
 });
